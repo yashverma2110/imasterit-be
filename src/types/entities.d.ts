a5b73390cd516630f2abc0d118b77be39a7358cb
yashverma2110 <yash.silver2110@gmail.com>
2023-03-31 12:55:34 +0530

    feat(types): added types for user and extended global express object

---
+++ src/types/entities.d.ts
@@ -0,0 +1,11 @@
+interface User {
+  _id: string;
+  telegramId: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  isPremium: boolean;
+  photo: string;
+  createdAt: string;
+  updatedAt: string;
+}
